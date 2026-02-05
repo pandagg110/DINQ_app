795d8c126844c44c78a4b3969e1a9acaed8dac18
songjinzhou <songjinzhou1@163.com>
2026-02-05 14:10:11 +0800

    feat: 启动 logo 动画

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,6 +1,6 @@
-import 'package:dinq_app/utils/color_util.dart';
-import 'package:dinq_app/widgets/common/base_page.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/common/lottie_view.dart';
 
 /// 启动页 - 在 UserStore 初始化完成前显示
 class SplashPage extends StatelessWidget {
@@ -11,23 +11,26 @@ class SplashPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AssetImageView('logo_white', width: 48, height: 48),
-            const SizedBox(width: 12),
-            Text(
-              'DINQ',
-              style: TextStyle(
-                fontSize: 36,
-                color: ColorUtil.textColor,
-                fontFamily: 'Geist',
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ],
-        ),
+        child: SizedBox(height: 50, child: const LottieView(asset: 'animations/splash_logo.json')),
       ),
+      // body: Center(
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       AssetImageView('logo_white', width: 48, height: 48),
+      //       const SizedBox(width: 12),
+      //       Text(
+      //         'DINQ',
+      //         style: TextStyle(
+      //           fontSize: 36,
+      //           color: ColorUtil.textColor,
+      //           fontFamily: 'Geist',
+      //           fontWeight: FontWeight.w600,
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
